48ce710e271d5d4a5fa7a3c1e95e715c937e2f68
y00801659 <yutianqi@huawei.com>
2022-12-22 17:11:42 +0800

    Update

--- support/expect_param_support.py
+++ support/expect_param_support.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import json
 
 class ExpectParamSupport():
     workPath = os.path.dirname(sys.argv[0])
@@ -14,8 +15,6 @@ class ExpectParamSupport():
     @classmethod
     def getParams(cls, node):
         (nodesCounter, params) = cls.getParamsFromNode(node)
-        params.insert(0, str(nodesCounter))
-        params.insert(1, "1")
         return " ".join(params)
 
     @classmethod
@@ -26,22 +25,50 @@ class ExpectParamSupport():
             node.get("username"),
             node.get("ip"),
             node.get("port"),
-            "0",
-            "1",
-            "1",
-            node.get("password")
+            '"' + node.get("password") + '"'
         ]
         if node.get("commands"):
-            commands = ['"' + item + '"' for item in node.get("commands")]
+            commands = []
+            for item in node.get("commands"):
+                expect = item.get("expect")
+                send = item.get("send")
+                commands.append('"' + expect + '"')
+                commands.append('"' + send + '"')
             params.append(str(len(commands)))
             params.extend(commands)
         else:
             params.append("0")
-        params.append("0")
 
         if node.get("jumper"):
             (subLevels, subParams) = cls.getParamsFromNode(node.get("jumper"))
             levels += subLevels
             params.extend(subParams)
         return (levels, params)
+
+
+
+if __name__ == "__main__":
+    rawJson = """{
+                    "nodeName": "Disb1_1",
+                    "nodeType": "session",
+                    "ip": "10.50.135.106",
+                    "port": "22",
+                    "username": "ossuser",
+                    "password": "Huawei@Cloud8#",
+                    "jumper": {
+                        "ip": "10.50.135.106",
+                        "port": "22",
+                        "username": "ossadm",
+                        "password": "Huawei@Cloud8#",
+                        "commands": [
+                            {"expect": "$ ","send": "su -"},
+                            {"expect": "Password","send": "Huawei@Cloud8#"},
+                            {"expect": "# ","send": "su - dbuser"}
+                        ]
+                    },
+                    "nodeId": 31
+                }"""
+    node = json.loads(rawJson)
+    # expect t.exp ssh ossuser 10.50.135.106 22 "Huawei@Cloud8#" 0 ssh ossadm 10.50.135.106 22 "Huawei@Cloud8#" 6 "$ " "su -" "Password" "Huawei@Cloud8#" "# " "su - dbuser"
+    print(ExpectParamSupport.getCmd(node))
 
